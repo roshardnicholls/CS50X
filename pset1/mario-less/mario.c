e3f3bbc47851a87beff479942a44ff3a56a03d3a
Roshard Nicholls <43711743+roshardnicholls@users.noreply.github.com>
2022-01-13 23:43:25 +0000

    Thu, Jan 13, 2022, 11:43 PM +00:00

--- pset1/mario-less/mario.c
+++ pset1/mario-less/mario.c
@@ -8,7 +8,7 @@ int main(void)
     {
         height = get_int("Enter pyramid height: ");
     }
-    while (height < 1);
+    while (height < 1  || height > 8);
 
     printf("Height = %i\n", height);
 }
