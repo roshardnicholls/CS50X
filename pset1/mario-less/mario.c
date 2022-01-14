611de9bc10f0daa5baf0df18e332fa916cef02b4
Roshard Nicholls <43711743+roshardnicholls@users.noreply.github.com>
2022-01-14 00:08:47 +0000

    Fri, Jan 14, 2022, 12:08 AM +00:00

--- pset1/mario-less/mario.c
+++ pset1/mario-less/mario.c
@@ -4,11 +4,16 @@
 int main(void)
 {
     int height;
+
+    // Ask user for pyramid height, re-prompt if height is not between 1 and 8
     do
     {
         height = get_int("Enter pyramid height: ");
     }
     while (height < 1  || height > 8);
 
-    printf("Height = %i\n", height);
+    for (int i = 0; i < height; i++)
+    {
+        printf("#\n");
+    }
 }
