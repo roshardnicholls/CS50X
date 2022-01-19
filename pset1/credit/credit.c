c49dcd7ff63b9de1442786407deb9b77c5eec814
Roshard Nicholls <43711743+roshardnicholls@users.noreply.github.com>
2022-01-19 02:47:59 +0000

    Wed, Jan 19, 2022, 2:47 AM +00:00

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -19,6 +19,7 @@ void validate_card(long card_number)
     int card_length = 0;
     int luhn_part1 = 0;
     int luhn_part2 = 0;
+    int luhn_total = 0;
     // Loop over the card number starting from the end
     while (card_number != 0)
     {
@@ -27,7 +28,6 @@ void validate_card(long card_number)
 
         // Implementing Luhn's Algorithm
          // Sum of doubled digits
-        // int luhn_total = 0;
 
         // Every other digit starting from second to last
         if (card_length % 2 == 0)
@@ -53,6 +53,8 @@ void validate_card(long card_number)
         // Remove the last digit so that we can use the next digit on following loop
         card_number /= 10;
     }
-    printf("Luhn Part 1: %i\n", luhn_part1);
-    printf("Luhn Part 2: %i\n", luhn_part2);
+    // printf("Luhn Part 1: %i\n", luhn_part1);
+    // printf("Luhn Part 2: %i\n", luhn_part2);
+
+    luhn_total = luhn_part1 + luhn part2;
 }
