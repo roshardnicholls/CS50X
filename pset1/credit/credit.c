3d30e10e888f2f2a6a0a350702b47e8a21497708
Roshard Nicholls <43711743+roshardnicholls@users.noreply.github.com>
2022-01-19 00:32:27 +0000

    Wed, Jan 19, 2022, 12:32 AM +00:00

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <cs50.h>
 
+void validate_card(long card_number);
+
+int main(void)
+{
+    // Ask user for card number
+    long card_number = get_long("Enter the card number: ");
+
+
+    validate_card(card_number);
+}
+
+// check if the number the user entered is a valid credit card number
+void validate_card(long card_number)
+{
+    int current_digit
+    while (current_digit / 10 != 0)
+    {
+        printf(current_digit)
+    }
+}
