da0d9318e6b47242b0185092e4c04a816dc10d88
Roshard Nicholls <43711743+roshardnicholls@users.noreply.github.com>
2022-01-25 12:16:20 +0000

    Tue, Jan 25, 2022, 12:16 PM +00:00

--- week2/scrabble/scrabble.c
+++ week2/scrabble/scrabble.c
@@ -26,7 +26,7 @@ int main(void)
 int compute_score(string word)
 {
     // TODO: Compute and return score for string
-    n = strlen(word);
+    int n = strlen(word);
     int letter_index;
     int score = 0;
     for (int i = 0; i < n; i++)
