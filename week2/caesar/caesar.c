e2ca25556b64505be00ca2c5ac7d1f70850a2917
Roshard Nicholls <43711743+roshardnicholls@users.noreply.github.com>
2022-02-09 14:37:16 +0000

    Wed, Feb 9, 2022, 2:37 PM +00:00

--- week2/caesar/caesar.c
+++ week2/caesar/caesar.c
@@ -1,5 +1,8 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <ctype.h>
+
+bool only_digits(string text);
 
 int main(int argc, string argv[])
 {
@@ -9,5 +12,18 @@ int main(int argc, string argv[])
         return 1;
     }
 
+
+
     return 0;
+}
+
+bool only_digits(string text);
+{
+    for (int i = 0, n = strlen(argv); i < n; i++)
+    {
+        if(!isdigit(argv[i]))
+        {
+
+        }
+    }
 }
