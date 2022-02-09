25357eda9e622bfa81ad757bdadf565e664ad38d
Roshard Nicholls <43711743+roshardnicholls@users.noreply.github.com>
2022-02-09 02:33:59 +0000

    Wed, Feb 9, 2022, 2:33 AM +00:00

--- week2/readability/readability.c
+++ week2/readability/readability.c
@@ -22,12 +22,12 @@ int main(void)
 
     if (index < 1)
     {
-        printf("Before Grade 1");
+        printf("Before Grade 1\n");
     }
 
     else if (index > 15.5)
     {
-        printf("Grade 16+");
+        printf("Grade 16+\n");
     }
 
     else
