45d8498b3114e1735fd99bd8eb86f02779329fdc
Roshard Nicholls <43711743+roshardnicholls@users.noreply.github.com>
2022-02-09 03:54:35 +0000

    Wed, Feb 9, 2022, 3:54 AM +00:00

--- week2/caesar/caesar.c
+++ week2/caesar/caesar.c
@@ -1,7 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
+    if (argc != 1)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
 
+    return 0;
 }
