4a244725ad7d3c3fa6b8cd6bb54230a685f0d33d
Roshard Nicholls <43711743+roshardnicholls@users.noreply.github.com>
2022-02-13 17:48:15 +0000

    Sun, Feb 13, 2022, 5:48 PM +00:00

--- week2/substitution/substitution.c
+++ week2/substitution/substitution.c
@@ -1,6 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 bool validate_key(key);
 // char substitute(char c, string k);
@@ -15,4 +16,44 @@ int main(int argc, string argv[])
     }
 
     bool valid = validate_key(argv[1]);
+    if (!valid)
+    {
+        return 1;
+    }
+}
+
+bool validate_key(string k)
+{
+    int n = strlen(k);
+    if (n != 26)
+    {
+        printf("Key must contain 26 characters.\n");
+        return false;
+    }
+
+    else
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (!isalpha(k[i]))
+            {
+                printf("Key must contain letters only.\n");
+                return false;
+            }
+
+            else
+            {
+                for (int j = i; j < n; j++)
+                {
+                    if (k[j] == k[i])
+                    {
+                        printf("Key must contain non-repeating characters.\n");
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+
+    return true;
 }
