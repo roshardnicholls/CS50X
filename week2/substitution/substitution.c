53457321a799c0f9fe89da125bf56b7757fc9e61
Roshard Nicholls <43711743+roshardnicholls@users.noreply.github.com>
2022-02-13 17:04:14 +0000

    Sun, Feb 13, 2022, 5:04 PM +00:00

--- week2/substitution/substitution.c
+++ week2/substitution/substitution.c
@@ -1,13 +1,18 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 // bool validate_key(key);
 // char substitute(char c, string k);
 
 int main(int argc, string argv[])
 {
+    // If the user enters 2+ or 0 line arguments, print an error message and stop the program
     if (argc = != 2)
     {
-        print("Pleas enter 1 command-line argument.\n");
+        print("Please enter 1 command-line argument.\n");
+        return 1;
     }
+
+    string key = argv[];
 }
