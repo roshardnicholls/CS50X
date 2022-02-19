f88704138508753a3cd6c8b3ba0c38e9970574c6
Roshard Nicholls <43711743+roshardnicholls@users.noreply.github.com>
2022-02-19 16:41:31 +0000

    Sat, Feb 19, 2022, 4:41 PM +00:00

--- week2/caesar/caesar.c
+++ week2/caesar/caesar.c
@@ -9,23 +9,27 @@ char rotate(char c, int n);
 
 int main(int argc, string argv[])
 {
+    // Display error message if user enters 0 or 2+ command-line arguments
     if (argc != 2)
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
 
+    // Display error message if key contains non-digit characters
     bool key_check = only_digits(argv[1]);
-
     if (!key_check)
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
 
+    // Convert the key from a string to an int
     int key = atoi(argv[1]);
 
     string text = get_string("plaintext: ");
+
+    // Call the rotate function to rotate each alphabetical character by "key"
     printf("ciphertext: ");
     for (int i = 0, n = strlen(text); i < n; i++)
     {
@@ -34,8 +38,6 @@ int main(int argc, string argv[])
     }
 
     printf("\n");
-
-
     return 0;
 }
 
@@ -48,6 +50,7 @@ bool only_digits(string s)
             continue;
         }
 
+        // If the character is not a digit, immediately return false
         else
         {
             return false;
@@ -59,8 +62,11 @@ bool only_digits(string s)
 char rotate(char c, int n)
 {
     char rotated_letter;
+
+    // Only rotate alphabetical characters
     if(isalpha(c))
     {
+        // Shift the character by n using ASCII values and return the rotated letter
         if(islower(c))
         {
             rotated_letter = ((((c - 97) + n) % 26) + 97);
