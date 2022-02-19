f95469dc130bbcb029c4c69fe840d0ccd64f7938
Roshard Nicholls <43711743+roshardnicholls@users.noreply.github.com>
2022-02-19 17:59:57 +0000

    Sat, Feb 19, 2022, 5:59 PM +00:00

--- week2/substitution/substitution.c
+++ week2/substitution/substitution.c
@@ -15,7 +15,8 @@ int main(int argc, string argv[])
         return 1;
     }
 
-    // If the key is invalid, print an error message and stop the program
+    // If the key is invalid, stop the program
+    // Error messages are handled by the validate_key function
     bool valid = validate_key(argv[1]);
     if (!valid)
     {
@@ -27,19 +28,19 @@ int main(int argc, string argv[])
 
     for (int i = 0, n = strlen(plaintext); i < n; i++)
     {
+        // Substitute each alphabetic char with the corresponding char in the key
         if (isalpha(plaintext[i]))
         {
             char cipherchar = substitute(plaintext[i], argv[1]);
             printf("%c", cipherchar);
         }
 
+        // Leave non-alphabetic characters unchanged
         else
         {
             printf("%c", plaintext[i]);
         }
-
     }
-
     printf("\n");
 
     return 0;
@@ -48,6 +49,8 @@ int main(int argc, string argv[])
 bool validate_key(string k)
 {
     int n = strlen(k);
+
+    // Reject all keys that are not 26 chars
     if (n != 26)
     {
         printf("Key must contain 26 characters.\n");
@@ -58,6 +61,7 @@ bool validate_key(string k)
     {
         for (int i = 0; i < n; i++)
         {
+            // Reject all keys that contain non-alphabetic chars
             if (!isalpha(k[i]))
             {
                 printf("Key must contain letters only.\n");
@@ -68,6 +72,7 @@ bool validate_key(string k)
             {
                 for (int j = i + 1; j < n; j++)
                 {
+                    // Reject all keys that contain repeating chars
                     if (k[j] == k[i])
                     {
                         printf("Key must contain non-repeating characters.\n");
@@ -77,30 +82,34 @@ bool validate_key(string k)
             }
         }
     }
-
     return true;
 }
 
 char substitute(char c, string k)
 {
     char substituted_letter;
+
+    // Avoid case conversions later by declaring both uppercase and lowercase alphabets as strings
     string upper_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     string lower_alphabet = "abcdefghijklmnopqrstuvwxyz";
-    int target_position;
+
+    int target_position; // Used to match position of original snad substituted chars
     bool target_found = false;
     int counter = 0;
 
     while (!target_found)
     {
+        // Once original char's position is found, set target_found to true and break out of the loop
         if (c == upper_alphabet[counter] || c == lower_alphabet[counter])
         {
             target_position = counter;
             target_found = true;
         }
-
         counter++;
     }
 
+    // Convert the char in key to corresponding case of the original char
+    // Return the substituted letter
     if (islower(c))
     {
         substituted_letter = tolower(k[target_position]);
