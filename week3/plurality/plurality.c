f6a44ff69eb86fa79ea7689ecda4b2ab17aafa85
Roshard Nicholls <43711743+roshardnicholls@users.noreply.github.com>
2022-02-23 16:33:10 +0000

    Wed, Feb 23, 2022, 4:33 PM +00:00

--- week3/plurality/plurality.c
+++ week3/plurality/plurality.c
@@ -87,7 +87,6 @@ void print_winner(void)
     else
     {
         int max_votes = candidates[0].votes;
-        int temp;
         for (int i = 0; i < candidate_count - 1; i++)
         {
              if (candidates[i].votes <= candidates[i+1].votes)
