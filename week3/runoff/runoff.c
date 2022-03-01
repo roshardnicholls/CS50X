805945532351521fcd7deab2cea800d7b6398908
Roshard Nicholls <43711743+roshardnicholls@users.noreply.github.com>
2022-03-01 00:34:34 +0000

    Tue, Mar 1, 2022, 12:34 AM +00:00

--- week3/runoff/runoff.c
+++ week3/runoff/runoff.c
@@ -148,20 +148,15 @@ void tabulate(void)
 
     for (int i = 0; i < voter_count; i++)
     {
-        // for (int j = 0; j < candidate_count; j++)
-        // {
-        //     // Connect the choice on the ballot to the corresponding candidate
-        //     candidate_index = preferences[i][j];
-        //     if (candidates[candidate_index].eliminated == false)
-        //     {
-        //         break;
-        //     }
-        //     else
-        //         j++;
-
-        //     candidates[candidate_index].votes++;
-        }
-
+        for (int j = 0; j < candidate_count; j++)
+        {
+            // Connect the choice on the ballot to the corresponding candidate
+            candidate_index = preferences[i][j];
+            if (candidates[candidate_index].eliminated == false)
+            {
+                candidates[candidate_index].votes++;
+                break;
+            }
     }
 
     return;
