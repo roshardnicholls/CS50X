dabe169ef691fe36f432aafe0d0d64989c216e19
Roshard Nicholls <43711743+roshardnicholls@users.noreply.github.com>
2022-04-25 20:51:03 +0000

    Mon, Apr 25, 2022, 8:51 PM +00:00

--- week3/runoff/runoff.c
+++ week3/runoff/runoff.c
@@ -141,7 +141,13 @@ bool vote(int voter, int rank, string name)
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
-    for (int i = 0; i < voter co)
+    for (int i = 0; i < voter_count; i++)
+    {
+        for (int j = 0; j < candidate_count; j++)
+        {
+            
+        }
+    }
     return;
 }
 
