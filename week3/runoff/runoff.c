dc633dcbd39924e5103f7fb21e4d296b26018e62
Roshard Nicholls <43711743+roshardnicholls@users.noreply.github.com>
2022-04-26 15:55:32 +0000

    Tue, Apr 26, 2022, 3:55 PM +00:00

--- week3/runoff/runoff.c
+++ week3/runoff/runoff.c
@@ -166,7 +166,7 @@ bool print_winner(void)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if ((float)(candidates[i].votes / voter_count) > 0.50)
+        if ((float) candidates[i].votes / voter_count > 0.50)
         {
             printf("%s\n", candidates[i].name);
             return true;
