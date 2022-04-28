36261dd42fbcbf773bd7451c5d88bc17b0b1fbbe
Roshard Nicholls <43711743+roshardnicholls@users.noreply.github.com>
2022-04-28 02:19:12 +0000

    Thu, Apr 28, 2022, 2:19 AM +00:00

--- week3/runoff/runoff.c
+++ week3/runoff/runoff.c
@@ -207,9 +207,12 @@ bool is_tie(int min)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        
+        if (candidates[i].votes != min && !candidates[i].eliminated)
+        {
+            return false;
+        }
     }
-    return false;
+    return true;
 }
 
 // Eliminate the candidate (or candidates) in last place
