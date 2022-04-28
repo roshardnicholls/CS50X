1be7b87bd41c46716bddc34f6336a6c0e9e08b1d
Roshard Nicholls <43711743+roshardnicholls@users.noreply.github.com>
2022-04-28 00:56:50 +0000

    Thu, Apr 28, 2022, 12:56 AM +00:00

--- week3/runoff/runoff.c
+++ week3/runoff/runoff.c
@@ -186,7 +186,7 @@ int find_min(void)
     {
         min_votes = candidates[n].votes;
     }
-    while (n < candidate_count && candidates[n].eliminated = true)
+    while (n < candidate_count && candidates[n].eliminated);
 
     // Calculate the minimum number of votes, skipping over any eliminated candidates
     for (int i = 0; i < candidate_count - 1; i++)
@@ -195,7 +195,7 @@ int find_min(void)
         {
             if (min_votes > candidates[i + 1].votes)
             {
-                min_votes > candidates[i + 1].votes;
+                min_votes = candidates[i + 1].votes;
             }
         }
     }
